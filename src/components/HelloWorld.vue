ec9acf3021ee750e5bd3ae2dcddcae9864bcc9f5
David <dsxvqq@gmail.com>
2024-01-11 00:24:41 +0600

    hello world page added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="hello">
+        <button v-if="!textVisible" @click="showHelloWorld" class="hello-world-button">click me 🙂</button>
+        <p class="hello-world-text">{{ responseText }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            responseText: '',
+            textVisible: false,
+        };
+    },
+    methods: {
+        async showHelloWorld() {
+            try {
+                const res = await fetch('http://194.152.37.7:8812/api/hello');
+                if (res.ok) {
+                    this.responseText = await res.text();
+                    this.textVisible = true;
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    },
+};
+</script>
+
+<style scoped>
+    button {
+        width: 300px;
+        height: 150px;
+        font-size: 50px;
+    }
+    p {
+        margin-top: 115px;
+        font-size: 50px;
+    }
+</style>
