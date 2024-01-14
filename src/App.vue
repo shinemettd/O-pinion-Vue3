dd22d11a9c1314383206ee6ad02855cc9de2db22
David <dsxvqq@gmail.com>
2024-01-15 00:23:54 +0600

    migrated from CLI to Vite

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
 <template>
-  <HelloWorld/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+  //
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="hello">
-        <button v-if="!textVisible" @click="showHelloWorld" class="hello-world-button">click me 🙂</button>
-        <p class="hello-world-text">{{ responseText }}</p>
-    </div>
+  <div class = "button-container">
+    <v-btn v-if = "!firstTextVisible" @click = "showText('first')" class = "custom-btn">Get from first API</v-btn>
+    <h1 class = "custom-text"> {{ firstResponseText }}</h1>
+    <v-btn v-if = "!secondTextVisible" @click = "showText('second')" class = "custom-btn">Get from second API</v-btn>
+    <h1 class = "custom-text"> {{ secondResponseText }}</h1>
+  </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            responseText: '',
-            textVisible: false,
-        };
-    },
-    methods: {
-        async showHelloWorld() {
-            try {
-                const res = await fetch('http://194.152.37.7:8812/');
-                if (res.ok) {
-                    this.responseText = await res.text();
-                    this.textVisible = true;
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        },
-    },
-};
+<script setup>
+  import {ref} from 'vue';
+  const firstTextVisible = ref(false);
+  const secondTextVisible = ref(false);
+  const firstResponseText = ref('');
+  const secondResponseText = ref('');
+  async function showText(value) {
+    const response = value === 'first' ? await fetch('http://194.152.37.7:8812/') : await fetch('http://194.152.37.7:7712/');
+    if (response.ok) {
+      if (value === 'first') {
+        firstTextVisible.value = true;
+        firstResponseText.value = await response.text();
+      } else {
+        secondTextVisible.value = true;
+        secondResponseText.value = await response.text();
+      } 
+    } 
+  }
 </script>
 
 <style scoped>
-    button {
-        width: 300px;
-        height: 150px;
-        font-size: 50px;
-    }
-    p {
-        margin-top: 115px;
-        font-size: 50px;
-    }
+.button-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.custom-btn,
+.custom-text {
+  margin-top: 25px;
+}
+
 </style>
