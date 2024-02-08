59d50ef4f9e2286b8df93e6040027aa007e3bf4a
kimgalina <genrietta.kim@gmail.com>
2024-02-08 21:40:35 +0600

    Added functionality for text align and for order and unorder lists

--- src/components/ArticleCreate3.vue
+++ src/components/ArticleCreate3.vue
@@ -20,12 +20,21 @@
         <div class="editor-tool">
             <div v-if="editor">
                 <input type="file" ref="fileInputRef" style="display: none;" @change="addImage">
-                <img src="/public/icons/upload_img.svg" class="btn-toolbar img" alt="icon"  @click="openFileInput">
+                <img src="/public/icons/upload_img.svg" class="btn-toolbar icon" alt="icon"  @click="openFileInput">
 
                 <button class="btn-toolbar bold"  @click="toggleBold">B</button>
                 <button class="btn-toolbar italic" @click="toggleItalic">I</button>
                 <button class="btn-toolbar underline" @click="toggleUnderline">U</button>
                 <button class="btn-toolbar strikethrough" @click="toggleStrike">S</button>
+                <button class="btn-toolbar link" @click="toggleLink">🔗</button>
+                <img src="/public/icons/align_left.svg" class="btn-toolbar icon" alt="icon"  @click="editor.chain().focus().setTextAlign('left').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }">
+                <img src="/public/icons/align_justify.svg" class="btn-toolbar icon" alt="icon" @click="editor.chain().focus().setTextAlign('justify').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'justify' }) }">
+                <img src="/public/icons/align_center.svg" class="btn-toolbar icon" alt="icon" @click="editor.chain().focus().setTextAlign('center').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }">
+                <img src="/public/icons/align_right.svg" class="btn-toolbar icon" alt="icon" @click="editor.chain().focus().setTextAlign('right').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }">
+
+                <img src="/public/icons/list-ul.svg" class="btn-toolbar icon" alt="icon" @click="editor.chain().focus().toggleBulletList().run()">
+                <img src="/public/icons/list-ol.svg" class="btn-toolbar icon" alt="icon" @click="editor.chain().focus().toggleOrderedList().run()">
+               
             </div>
         </div>
         <editor-content :editor="editor" class="custom-editor"/>
@@ -41,12 +50,19 @@
   import Dropcursor from '@tiptap/extension-dropcursor'
   import Paragraph from '@tiptap/extension-paragraph'
   import Text from '@tiptap/extension-text'
+  import Heading from '@tiptap/extension-heading'
 
   import Image from '@tiptap/extension-image'
   import Bold from '@tiptap/extension-bold'
   import Italic from '@tiptap/extension-italic'
   import Strike from '@tiptap/extension-strike'
   import Underline from '@tiptap/extension-underline'
+  import Link from '@tiptap/extension-link'
+  import TextAlign from '@tiptap/extension-text-align'
+  import ListItem from '@tiptap/extension-list-item'
+  import BulletList from '@tiptap/extension-bullet-list'
+  import OrderedList from '@tiptap/extension-ordered-list'
+
 
   import { Editor, EditorContent } from '@tiptap/vue-3'
   import axios from 'axios';
@@ -70,12 +86,23 @@ export default {
         Document,
         Paragraph,
         Text,
+        Heading,
+        TextAlign.configure({
+          types: ['heading', 'paragraph'],
+        }),
         Image,
         Dropcursor,
         Bold,
         Italic,
         Strike,
         Underline,
+        Link.configure({
+          openOnClick: true,
+        }),
+        BulletList,
+        ListItem,
+        OrderedList,
+       
       ],
       content: `
         <p>This is a basic example of implementing images. </p>
@@ -88,6 +115,36 @@ export default {
       fileInputRef.value = document.querySelector('input[type="file"]');
     });
     
+    const setLink = () => {
+      const previousUrl = editor.getAttributes('link').href
+      const url = window.prompt('URL', previousUrl)
+
+      // cancelled
+      if (url === null) {
+        return
+      }
+
+      // empty
+      if (url === '') {
+        editor
+          .chain()
+          .focus()
+          .extendMarkRange('link')
+          .unsetLink()
+          .run()
+
+        return
+      }
+
+      // update link
+      editor
+        .chain()
+        .focus()
+        .extendMarkRange('link')
+        .setLink({ href: url })
+        .run()
+    };
+
      // Функция для переключения стиля жирного текста
     const toggleBold = () => {
       if (editor) {
@@ -125,6 +182,16 @@ export default {
           editor.chain().focus().unsetUnderline().run();
         } else {
           editor.chain().focus().setUnderline().run();
+        }
+      }
+    };
+
+    const toggleLink = () => {
+      if (editor) {
+        if (editor.isActive('link')) {
+          editor.chain().focus().unsetLink().run();
+        } else {
+          setLink();
         }
       }
     };
@@ -234,12 +301,14 @@ export default {
       toggleBold,
       toggleItalic,
       toggleStrike,
-      toggleUnderline
+      toggleUnderline,
+      toggleLink,
      
     };
   },
+
   beforeUnmount() {
-    this.editor.destroy();
+    editor.destroy();
   },
 };
 
@@ -247,6 +316,7 @@ export default {
   </script>
   
   <style scoped>
+
   .article-container {
     background-color: #f8f8f8;
     padding: 20px;
@@ -313,6 +383,7 @@ export default {
     max-height: 400px; /* Максимальная высота редактора */
     overflow-y: auto; /* Появляется вертикальный скролл, если контент выходит за пределы редактора */
     line-height: 2;
+    padding: 20px;
     
   }
   .editor-tool {
@@ -338,7 +409,7 @@ export default {
   background-color: #eaeaea;
   }
 
-  .img {
+  .icon {
     display: inline;
   }
 
@@ -357,6 +428,9 @@ export default {
 .strikethrough {
   text-decoration: line-through;
 }
+
+
+
 
 </style>
   
