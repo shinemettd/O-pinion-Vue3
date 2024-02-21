4bf8a4b64bdcbfe7af470eda3e7097ab1e1824a8
David <dsxvqq@gmail.com>
2024-02-22 00:24:36 +0600

    updated user profile

--- src/pages/UserProfilePage.vue
+++ src/pages/UserProfilePage.vue
@@ -4,13 +4,20 @@ import UserProfileComponent from "@/components/UserProfileComponent.vue";
 import axios from "axios";
 import {useRoute} from "vue-router";
 import {onBeforeMount, ref} from "vue";
+import PageNotFound from "@/pages/PageNotFound.vue";
 const route = useRoute();
 const userNickname = route.params.userNickname;
 const user = ref([]);
+const isUserValid = ref();
 
 const getUser = async () => {
-  let response = await axios.get(`http://194.152.37.7:8812/api/users/nickname/${userNickname}/profile`);
-  user.value = response.data;
+  try {
+    let response = await axios.get(`http://194.152.37.7:8812/api/users/nickname/${userNickname}/profile`);
+    user.value = response.data;
+    isUserValid.value = true;
+  } catch (e) {
+    isUserValid.value = false;
+  }
 }
 
 onBeforeMount(() => {
@@ -19,19 +26,22 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <UserProfileComponent
-    :user-id="user.id"
-    :user-nickname="user.nickname"
-    :user-avatar-url="user.avatar || 'https://cdn-icons-png.flaticon.com/512/10/10938.png'"
-    :user-first-name="user.firstName"
-    :user-last-name="user.lastName"
-    :user-email="user.email"
-    :user-birthdate="user.birth_date"
-    :is-first-name-visible="user.is_first_name_visible"
-    :is-last-name-visible="user.is_last_name_visible"
-    :is-birthdate-visible="user.is_birth_date_visible"
-    :is-email-visible="user.is_email_visible"
-  />
+  <div v-if="isUserValid === true">
+    <UserProfileComponent
+                          :user-id="user.id"
+                          :user-nickname="user.nickname"
+                          :user-avatar-url="user.avatar || 'https://cdn-icons-png.flaticon.com/512/10/10938.png'"
+                          :user-first-name="user.firstName"
+                          :user-last-name="user.lastName"
+                          :user-email="user.email"
+                          :user-birthdate="user.birthDate"
+    />
+  </div>
+  <div v-else-if="isUserValid === false">
+    <PageNotFound>
+
+    </PageNotFound>
+  </div>
 </template>
 
 <style scoped>
