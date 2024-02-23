13dbf6788f51789c4ae97a7bdb222e5959e769b5
David <dsxvqq@gmail.com>
2024-02-23 19:59:43 +0600

    added vuex and vuex-persistedstate dependencies

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import { registerPlugins } from '@/plugins';
 import App from './App.vue';
 
 import { createApp } from 'vue';
+import store from '@/store/store.js';
 
 const app = createApp(App);
 
 registerPlugins(app);
+app.use(store);
 app.mount('#app');
