c92903aa6f4181c5616f2937e6be9e6b4854320e
kimgalina <genrietta.kim@gmail.com>
2024-02-23 19:17:23 +0600

    Added constraints in editor according to html content

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -34,8 +34,6 @@
             <editor-content :editor="editor" class="custom-text-editor"/>
             <div class="character-count" v-if="editor">
             {{ editor.storage.characterCount.characters() }}/{{ limit }} characters
-            <br>
-            {{ editor.storage.characterCount.words() }} words
             </div>
         </div>
 </template>
