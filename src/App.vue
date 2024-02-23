52d1bfe5afc396d11a685dc53fb49517a8c808e1
David <dsxvqq@gmail.com>
2024-02-23 20:01:49 +0600

    added saving of user information after successful  login

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <Navbar/>
+      <Navbar :isUserAthorized = 'isUserAuthorized'/>
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import Navbar from "@/components/Navbar.vue";
+  import Navbar from "@/components/Navbar.vue";
+  import {useStore} from 'vuex';
+
+  const store = useStore();
+
+  const isUserAuthorized = store.state.isAuthorized;
 </script>
